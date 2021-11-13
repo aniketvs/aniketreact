cd47787c4b076110b887bcf99733e2740c8306fd
aniket sharma <72964172+aniketvs@users.noreply.github.com>
2021-11-13 20:36:57 +0530

    Add files via upload

---
+++ precache-manifest.f51e77232cb67c47cf071e015a5bc484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f605c765d1f7889f4507554c4ec645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5521564f61f8aea6246d",
+    "url": "/static/css/main.995457f0.chunk.css"
+  },
+  {
+    "revision": "bd131b6ef6e01c306569",
+    "url": "/static/js/2.96e0a854.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/static/js/2.96e0a854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1c3c5471681485d0dc",
+    "url": "/static/js/3.1e5f95b2.chunk.js"
+  },
+  {
+    "revision": "5521564f61f8aea6246d",
+    "url": "/static/js/main.766dfdd0.chunk.js"
+  },
+  {
+    "revision": "c6da1bdebcc37725f11b",
+    "url": "/static/js/runtime-main.48bfe0d7.js"
+  }
+]);
